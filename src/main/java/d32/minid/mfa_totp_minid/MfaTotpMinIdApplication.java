1c2ad3b536f6c789be10128cd3fc96a883cf7ef2
UlrikFa <595440@stud.hvl.no>
2023-03-21 09:31:59 +0100

    start

---
+++ src/main/java/d32/minid/mfa_totp_minid/MfaTotpMinIdApplication.java
@@ -0,0 +1,13 @@
+package d32.minid.mfa_totp_minid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfaTotpMinIdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfaTotpMinIdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/d32/minid/mfa_totp_minid/MfaTotpMinIdApplicationTests.java
@@ -0,0 +1,13 @@
+package d32.minid.mfa_totp_minid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfaTotpMinIdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
