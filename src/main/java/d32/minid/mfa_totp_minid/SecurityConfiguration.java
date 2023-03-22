6959b8f2010ab0a06195b342291c0dddf3fc4587
Jørgen Vikan Eidsvåg <69847918+AdmiralSilver@users.noreply.github.com>
2023-03-22 10:59:18 +0100

    added securityconfig

---
+++ src/main/java/d32/minid/mfa_totp_minid/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package d32.minid.mfa_totp_minid;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
+}
--- src/main/java/d32/minid/mfa_totp_minid/controllers/LoginController.java
+++ src/main/java/d32/minid/mfa_totp_minid/controllers/LoginController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    @GetMapping("/asd")
+    @GetMapping("/login")
     public String login() {
-        return "asd";
+        return "login";
     }
 }
