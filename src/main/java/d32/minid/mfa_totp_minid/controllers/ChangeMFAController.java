dc65a5c4412cdcd7982ce539c6a9a01c40ba18f6
Feriksdal <69585779+Feriksdal@users.noreply.github.com>
2023-04-04 12:15:14 +0200

    linked options-page

---
+++ src/main/java/d32/minid/mfa_totp_minid/controllers/ChangeMFAController.java
@@ -0,0 +1,27 @@
+package d32.minid.mfa_totp_minid.controllers;
+
+import d32.minid.mfa_totp_minid.repository.UserRepository;
+import d32.minid.mfa_totp_minid.user.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class ChangeMFAController {
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("/mfa_options")
+    public String changeMFA(HttpSession session, @RegisteredOAuth2AuthorizedClient("idporten") OAuth2AuthorizedClient authorizedClient,Model model) {
+        String phone = "99887766";
+        User user = userRepository.findByPid((String) session.getAttribute("PID"));
+        String mfaMethod = user.getMfa_method();
+
+        model.addAttribute("chosen-mfa", mfaMethod);
+        model.addAttribute("phone", phone);
+
+        return "mfa_options";
+    }
+}
