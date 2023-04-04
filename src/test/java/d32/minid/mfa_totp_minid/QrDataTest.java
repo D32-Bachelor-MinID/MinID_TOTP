5c518968a20b712b2ef7afe2c48e02f9ed1a9573
Ulrik Fagerberg <69850787+h595440@users.noreply.github.com>
2023-04-04 10:46:49 +0200

    fix + sout removal (#22)

--- src/test/java/d32/minid/mfa_totp_minid/QrDataTest.java
+++ src/test/java/d32/minid/mfa_totp_minid/QrDataTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,14 +21,14 @@ public class QrDataTest {
     private final String key = "HMZARLOXOJHZ2FVAQMKDMO6L3O627AFR";
     @Before
     public void setUp() {
-        qrtest = new QrData("totp", "testesen@test.com", key, "DigDir MinID", "SHA1", 6, 30);
+        qrtest = new QrData("totp", "11223312345", key, "DigDir MinID", "SHA1", 6, 30);
         generator = new QrGenerator();
     }
     @Test
     public void testGenerator() throws IOException {
 /*        ByteArrayInputStream bis = new ByteArrayInputStream(generator.generate(qrtest));
         BufferedImage bImage = ImageIO.read(bis);
-        ImageIO.write(bImage, "png", new File("qrPic.png"));*/
-        assertEquals(qrtest.getUri(), "otpauth://totp/testesen%40test.com?secret="+key+"&issuer=DigDir%20MinID&algorithm=SHA1&digits=6&period=30");
+        ImageIO.write(bImage, "png", new File("qrPic.png"));*/ //generer en PNG for qrkode-testing
+        assertEquals(qrtest.getUri(), "otpauth://totp/11223312345?secret="+key+"&issuer=DigDir%20MinID&algorithm=SHA1&digits=6&period=30");
     }
 }
