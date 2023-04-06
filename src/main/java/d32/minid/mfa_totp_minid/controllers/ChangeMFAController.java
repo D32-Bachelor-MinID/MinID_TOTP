75bcd7e7578639ac1de945ee967f7e31542efae9
Erik Fiksdal <69585779+Feriksdal@users.noreply.github.com>
2023-04-06 09:38:25 +0200

    Settings mfa page (#27)
    * linked options-page
    
    * checked radiobuttons
    
    * updated for collaboration
    
    * Update application.properties
    
    * henter inn fra form og oppdaterer 2fa i db
    
    * Update application.properties

---
+++ src/main/java/d32/minid/mfa_totp_minid/controllers/ChangeMFAController.java
@@ -0,0 +1,56 @@
+package d32.minid.mfa_totp_minid.controllers;
+
+import d32.minid.mfa_totp_minid.repository.UserRepository;
+import d32.minid.mfa_totp_minid.user.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ChangeMFAController {
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("/mfa_options")
+    public String changeMFA(HttpSession session,Model model) {
+        String phone = "99887766";
+        User user = userRepository.findByPid((String) session.getAttribute("PID"));
+        String mfaMethod = user.getMfa_method();
+
+        boolean otc = false;
+        boolean totp = false;
+        boolean app = false;
+
+        switch (mfaMethod) {
+            case "OTC" -> otc = true;
+            case "TOTP" -> totp = true;
+            case "APP" -> app = true;
+        }
+        //display sms
+        //display app if registered else blank
+        //display totp if registered else link to register
+
+        model.addAttribute("chosen-mfa", mfaMethod);
+        model.addAttribute("otc", otc);
+        model.addAttribute("totp", totp);
+        model.addAttribute("app", app);
+        model.addAttribute("phone", phone);
+
+
+        System.out.println("get mfa_options");
+        return "mfa_options";
+    }
+
+    @PostMapping("/mfa_options")
+    public String changeMFA(HttpSession session, @RequestParam("chosen-mfa") String mfa) {
+
+        System.out.println("post mfa_options");
+
+
+        User user = userRepository.findByPid((String) session.getAttribute("PID"));
+        user.setMfa_method(mfa);
+        userRepository.save(user);
+        return "redirect:/settings";
+    }
+}
