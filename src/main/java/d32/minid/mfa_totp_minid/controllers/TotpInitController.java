97a5f547af39d43ae56ea06ab55e1b7ba07585ec
Ulrik Fagerberg <69850787+h595440@users.noreply.github.com>
2023-04-06 13:45:43 +0200

    Qrcode embeded in prog (#29)
    * help
    
    * riktig qrkode vises ved reg
    
    * postmapping for step2
    
    * html fix
    
    ---------
    
    Co-authored-by: Jørgen Vikan Eidsvåg <69847918+AdmiralSilver@users.noreply.github.com>

--- src/main/java/d32/minid/mfa_totp_minid/controllers/TotpInitController.java
+++ src/main/java/d32/minid/mfa_totp_minid/controllers/TotpInitController.java
@@ -1,10 +1,20 @@
 package d32.minid.mfa_totp_minid.controllers;
 
+import d32.minid.mfa_totp_minid.crypto.Crypto;
+import d32.minid.mfa_totp_minid.crypto.Totp;
+import d32.minid.mfa_totp_minid.qrcode.QrData;
+import d32.minid.mfa_totp_minid.qrcode.QrGenerator;
 import d32.minid.mfa_totp_minid.repository.CryptoRepository;
 import d32.minid.mfa_totp_minid.repository.UserRepository;
+import d32.minid.mfa_totp_minid.security.Validator;
+import d32.minid.mfa_totp_minid.time.TimeProvider;
+import d32.minid.mfa_totp_minid.user.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TotpInitController {
@@ -17,4 +27,36 @@ public class TotpInitController {
     public String totpinit() {
         return "totpinit";
     }
+
+    @GetMapping("/stepTwo")
+    public String stepTwo(HttpSession session, Model model) {
+        QrGenerator qrGenerator = new QrGenerator();
+        User user = userRepository.findByPid((String) session.getAttribute("PID"));
+        QrData qrData = qrGenerator.qrSetup(user);
+
+        cryptoRepository.save(new Crypto(user.getUuid(), qrData.getSecret(), ""));
+        model.addAttribute("dataUri", qrGenerator.getUriForImage(qrData, "image/png"));
+
+        return "totpReg";
+    }
+    @PostMapping ("/stepTwo")
+    public String stepTwoPost(String totpinput, HttpSession session, Model model) {
+        System.out.println("POST to stepTwo");
+        User user = userRepository.findByPid((String) session.getAttribute("PID"));
+        Crypto crypto = cryptoRepository.findByUuid(user.getUuid());
+        Validator validator = new Validator(new Totp(), new TimeProvider());
+        if (validator.validTotp(crypto, totpinput)) {
+            System.out.println("TOTP valid, saving user");
+            user.setMfa_method("TOTP");
+            userRepository.save(user);
+            return "redirect:/settings";
+        }
+        model.addAttribute("Error", "Invalid TOTP");
+        System.out.println("TOTP invalid, returning to totpReg");
+        QrData qr = new QrData("totp", user.getPid(), crypto.getSecret(), "MinID", "SHA1", 6, 30);
+        QrGenerator qrGenerator = new QrGenerator();
+        model.addAttribute("dataUri", qrGenerator.getUriForImage(qr, "image/png"));
+        return "totpReg";
+    }
+
 }
