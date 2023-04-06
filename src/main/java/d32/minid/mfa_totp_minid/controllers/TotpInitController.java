5228108efc402717ee1ed7a1a0c7475ad7c0f9ce
Jørgen Vikan Eidsvåg <69847918+AdmiralSilver@users.noreply.github.com>
2023-04-06 12:55:39 +0200

    postmapping for step2

--- src/main/java/d32/minid/mfa_totp_minid/controllers/TotpInitController.java
+++ src/main/java/d32/minid/mfa_totp_minid/controllers/TotpInitController.java
@@ -1,16 +1,20 @@
 package d32.minid.mfa_totp_minid.controllers;
 
 import d32.minid.mfa_totp_minid.crypto.Crypto;
+import d32.minid.mfa_totp_minid.crypto.Totp;
 import d32.minid.mfa_totp_minid.qrcode.QrData;
 import d32.minid.mfa_totp_minid.qrcode.QrGenerator;
 import d32.minid.mfa_totp_minid.repository.CryptoRepository;
 import d32.minid.mfa_totp_minid.repository.UserRepository;
+import d32.minid.mfa_totp_minid.security.Validator;
+import d32.minid.mfa_totp_minid.time.TimeProvider;
 import d32.minid.mfa_totp_minid.user.User;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TotpInitController {
@@ -35,4 +39,24 @@ public class TotpInitController {
 
         return "totpReg";
     }
+    @PostMapping ("/stepTwo")
+    public String stepTwoPost(String totpinput, HttpSession session, Model model) {
+        System.out.println("POST to stepTwo");
+        User user = userRepository.findByPid((String) session.getAttribute("PID"));
+        Crypto crypto = cryptoRepository.findByUuid(user.getUuid());
+        Validator validator = new Validator(new Totp(), new TimeProvider());
+        if (validator.validTotp(crypto, totpinput)) {
+            System.out.println("TOTP valid, saving user");
+            user.setMfa_method("TOTP");
+            userRepository.save(user);
+            return "redirect:/settings";
+        }
+        model.addAttribute("Error", "Invalid TOTP");
+        System.out.println("TOTP invalid, returning to totpReg");
+        QrData qr = new QrData("totp", user.getPid(), crypto.getSecret(), "MinID", "SHA1", 6, 30);
+        QrGenerator qrGenerator = new QrGenerator();
+        model.addAttribute("dataUri", qrGenerator.getUriForImage(qr, "image/png"));
+        return "totpReg";
+    }
+
 }
