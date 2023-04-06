2ad08157ab80d9b5406f49cffd185aa62c13129f
Ulrik Fagerberg <69850787+h595440@users.noreply.github.com>
2023-04-06 14:06:03 +0200

    qrdatatest og todos (#30)

--- src/main/java/d32/minid/mfa_totp_minid/controllers/SettingsController.java
+++ src/main/java/d32/minid/mfa_totp_minid/controllers/SettingsController.java
@@ -5,9 +5,6 @@ import d32.minid.mfa_totp_minid.security.SessionHandler;
 import d32.minid.mfa_totp_minid.user.User;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +14,8 @@ public class SettingsController {
     @Autowired
     UserRepository userRepository;
     @GetMapping("/settings")
-    public String settings(HttpSession session, @RegisteredOAuth2AuthorizedClient("idporten") OAuth2AuthorizedClient authorizedClient, Model model) {
-        OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+    public String settings(HttpSession session, Model model) {
         SessionHandler sessionHandler = new SessionHandler(session);
-        System.out.println("access token: " + accessToken.getTokenValue());
 
         if (!sessionHandler.hasAttribute(session)) {
             session.invalidate();
