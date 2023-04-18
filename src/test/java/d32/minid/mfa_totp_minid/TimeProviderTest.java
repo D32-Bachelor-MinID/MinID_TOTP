d58d8fe2576d0a1301e140fe80ccde990bc2e1be
Erik Fiksdal <69585779+Feriksdal@users.noreply.github.com>
2023-04-18 14:25:38 +0200

    testing & javadoc (#32)
    * Create TimeProviderTest.java
    
    * Create SessionHandlerTest.java
    
    * Javadocs for KeyGen

---
+++ src/test/java/d32/minid/mfa_totp_minid/TimeProviderTest.java
@@ -0,0 +1,24 @@
+package d32.minid.mfa_totp_minid;
+
+import d32.minid.mfa_totp_minid.time.TimeProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimeProviderTest {
+
+    TimeProvider timeProvider;
+
+    @Test
+    public void shouldReturnTime() {
+        timeProvider = new TimeProvider();
+        long time = timeProvider.getTime();
+        assertTrue(time > 0);
+    }
+
+}
