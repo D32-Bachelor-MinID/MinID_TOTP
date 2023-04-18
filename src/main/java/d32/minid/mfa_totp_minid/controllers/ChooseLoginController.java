d0cf50b73d89e4bb781f114f25ef2170565427a6
Jørgen Vikan Eidsvåg <69847918+AdmiralSilver@users.noreply.github.com>
2023-04-18 14:44:03 +0200

    RemoveApp working and rikig flow til innlogging

--- src/main/java/d32/minid/mfa_totp_minid/controllers/ChooseLoginController.java
+++ src/main/java/d32/minid/mfa_totp_minid/controllers/ChooseLoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChooseLoginController {
-    @GetMapping("/chooseLogin")
+    @GetMapping("/")
     public String chooseLogin() {
         return "chooseLogin";
     }
