a29f360d0c2acd7408942323f236398e83129208
Erik Fiksdal <69585779+Feriksdal@users.noreply.github.com>
2023-04-18 14:21:53 +0200

    Create SessionHandlerTest.java

---
+++ src/test/java/d32/minid/mfa_totp_minid/SessionHandlerTest.java
@@ -0,0 +1,44 @@
+package d32.minid.mfa_totp_minid;
+
+import d32.minid.mfa_totp_minid.security.SessionHandler;
+import jakarta.servlet.http.HttpSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpSession;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionHandlerTest {
+    HttpSession session;
+    SessionHandler sessionHandler;
+
+    @Test
+    public void hasAttributeShouldFindAttribute() {
+        session = new MockHttpSession();
+        session.setAttribute("PID", "12345678901");
+        sessionHandler = new SessionHandler(session);
+
+
+        assertTrue(sessionHandler.hasAttribute(session));
+    }
+
+    @Test
+    public void getSessionShouldReturnSession() {
+        session = new MockHttpSession();
+        session.setAttribute("PID", "12345678901");
+        sessionHandler = new SessionHandler(session);
+
+        assertNotNull(sessionHandler.getSession());
+        assertEquals(session, sessionHandler.getSession());
+
+    }
+
+    @Test
+    public void nullSessionShouldNotExist() {
+        session = null;
+        sessionHandler = new SessionHandler(session);
+
+        boolean result = sessionHandler.exists(session);
+
+        assertFalse(result);
+    }
+}
