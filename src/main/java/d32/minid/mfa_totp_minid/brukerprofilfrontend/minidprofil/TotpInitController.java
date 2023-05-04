c168c95d84effb6dc28ed9b7b1a7933a8cd16728
Jørgen Vikan Eidsvåg <69847918+AdmiralSilver@users.noreply.github.com>
2023-05-04 10:56:29 +0200

    Created utils class and updated controllers

--- src/main/java/d32/minid/mfa_totp_minid/brukerprofilfrontend/minidprofil/TotpInitController.java
+++ src/main/java/d32/minid/mfa_totp_minid/brukerprofilfrontend/minidprofil/TotpInitController.java
@@ -1,14 +1,6 @@
 package d32.minid.mfa_totp_minid.brukerprofilfrontend.minidprofil;
 
-import d32.minid.mfa_totp_minid.idportenbackend.minidprofil.Crypto;
-import d32.minid.mfa_totp_minid.idportenbackend.totpauthentication.totp.Totp;
-import d32.minid.mfa_totp_minid.idportenbackend.totpauthentication.qrcode.QrData;
-import d32.minid.mfa_totp_minid.idportenbackend.totpauthentication.qrcode.QrGenerator;
-import d32.minid.mfa_totp_minid.idportenbackend.DAO.repository.CryptoRepository;
-import d32.minid.mfa_totp_minid.idportenbackend.DAO.repository.UserRepository;
-import d32.minid.mfa_totp_minid.idportenbackend.totpauthentication.security.Validator;
-import d32.minid.mfa_totp_minid.idportenbackend.totpauthentication.totp.timeprovider.TimeProvider;
-import d32.minid.mfa_totp_minid.idportenbackend.minidprofil.User;
+import d32.minid.mfa_totp_minid.idportenbackend.utils.Utils;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
@@ -21,13 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class TotpInitController {
     @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private CryptoRepository cryptoRepository;
+    Utils utils;
 
     @GetMapping("/totpInit")
     public String totpinit(@RegisteredOAuth2AuthorizedClient("idporten") OAuth2AuthorizedClient authorizedClient, HttpSession session) {
-        if (session.getAttribute("PID") == null) {
+        if (!utils.sessionContainsPid(session)) {
             return "redirect:/loginn";
         }
         return "totpinit";
@@ -35,29 +25,20 @@ public class TotpInitController {
 
     @GetMapping("/stepTwo")
     public String stepTwo(HttpSession session, Model model) {
-        QrGenerator qrGenerator = new QrGenerator();
-        User user = userRepository.findByPid((String) session.getAttribute("PID"));
-        QrData qrData = qrGenerator.qrSetup(user);
 
-        cryptoRepository.save(new Crypto(user.getUuid(), qrData.getSecret(), ""));
-        model.addAttribute("dataUri", qrGenerator.getUriForImage(qrData, "image/png"));
+        model.addAttribute("dataUri", utils.generateQrCode(session));
 
         return "totpReg";
     }
     @PostMapping ("/stepTwo")
     public String stepTwoPost(String totpinput, HttpSession session, Model model) {
-        User user = userRepository.findByPid((String) session.getAttribute("PID"));
-        Crypto crypto = cryptoRepository.findByUuid(user.getUuid());
-        Validator validator = new Validator(new Totp(), new TimeProvider());
-        if (validator.validTotp(crypto, totpinput)) {
-            user.setMfa_method("TOTP");
-            userRepository.save(user);
+
+        if (utils.validateTotpSetup(totpinput,session)) {
+            utils.updateMfaMethod(session, "TOTP");
             return "redirect:/settings";
         }
         model.addAttribute("Error", "Invalid TOTP");
-        QrData qr = new QrData("totp", user.getPid(), crypto.getSecret(), "MinID", "SHA1", 6, 30);
-        QrGenerator qrGenerator = new QrGenerator();
-        model.addAttribute("dataUri", qrGenerator.getUriForImage(qr, "image/png"));
+        model.addAttribute("dataUri", utils.recreateQrCode(session));
         return "totpReg";
     }
 
